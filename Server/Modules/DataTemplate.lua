1460b90f1fec4c4d1204ac62e89ccd72d2092042
Dox0ed <ivanostrejko77@gmail.com>
2025-11-09 18:51:27 +0300

    Create DataTemplate.lua

---
+++ Server/Modules/DataTemplate.lua
@@ -0,0 +1,20 @@
+return table.freeze({
+	Cash = 0;
+	
+	FirstJoin = true;
+	
+	Towers = {};
+	
+	PurchasedPasses = {};
+	Stats = {
+		Luck = 1,
+		CashMultiplier = 1;
+	};
+	Spent = 0;
+	
+	Settings = {
+		Trades = 1;
+		Time = 1;
+		Music = 1;
+	};
+})
